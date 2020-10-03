2a09703eccc8ec1c377656a4858278de024417df
nandan-unni <asnqln@gmail.com>
2020-10-03 23:44:46 +0530

    Updates

---
+++ precache-manifest.f8ebf124029d5b297b9c5889efd3676f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47bbdc80d0d08a00c70306cade7824b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d6a22e51d099e0229e2",
+    "url": "/static/css/main.2059e464.chunk.css"
+  },
+  {
+    "revision": "418ba8987a3698d12492",
+    "url": "/static/js/2.f0b72ba7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f0b72ba7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d6a22e51d099e0229e2",
+    "url": "/static/js/main.8c501e13.chunk.js"
+  },
+  {
+    "revision": "2d4daf1fa9867e639b40",
+    "url": "/static/js/runtime-main.ec11ef69.js"
+  }
+]);
