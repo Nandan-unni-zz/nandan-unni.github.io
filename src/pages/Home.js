cd68da351ca90dbd67d963f8f8c0eb7788ef51d6
nandan-unni <asnqln@gmail.com>
2020-10-04 16:33:39 +0530

    added project animations

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Typist from 'react-typist';
+import FadeIn from 'react-fade-in';
 
 import './Home.css';
 import Footer from '../components/Footer';
@@ -9,113 +10,113 @@ import JavaScript from '../assets/javascript.png';
 import ReactLogo from '../assets/react.png';
 
 class Home extends Component {
-  render() {
-    return (
-      <div className="Home">
-        <div className="header">
-          <div className="name">
-            <i class="fa fa-2x fa-terminal">
-              <Typist 
-                  avgTypingDelay={100} 
-                  startDelay={200}
-                  cursor={{ element: ""}}>
-                  <b> Nandanunni</b>
-              </Typist>
-            </i>
-          </div>
-        </div><br />
-        <div className="projects-tab">
-
-          <div className="tab-section">
-            <div className="Card">
-                <h2>KeyBlogs API</h2>
-                <h4>Python, Django, Django Rest Framework, PostgreSQL, SQLite3</h4>
-                <p>KeyBlogs API is a blogging web app's API made with Django REST Framework
-                   API endpoints for CRUD operations on Users, Blogs with authentication, 
-                   email verification and searching users.
-                </p>
-                <div className="card-lang">
-                    <img src={Python} alt="Python" /> &nbsp; 
-                    <img src={Django} alt="Django" />
+    render() {
+        return (
+            <div className="Home">
+                <div className="header">
+                    <div className="name">
+                        <i class="fa fa-2x fa-terminal">
+                            <Typist 
+                                avgTypingDelay={100} 
+                                startDelay={200}
+                                cursor={{ element: "_"}}>
+                                <b> Nandanunni</b>
+                            </Typist>
+                        </i>
+                    </div>
                 </div><br />
-                <div className="card-nav">
-                    <div className="card-nav-left">
-                        <a href="https://github.com/nandan-unni/KeyBlogs-Django-Backend">View Source Code</a>
+                <FadeIn delay={1200} transitionDuration={1000}>
+                    <div className="projects-header">
+                        <i class="fa fa-2x fa-diamond"><b>&nbsp;Projects</b></i><br/>
+                        <div className="projects-line"></div>
                     </div>
-                    <div className="card-nav-right">
-                        <a href="https://keyblogs.web.app">View Project</a>
-                    </div>
-                </div>
-            </div> &nbsp; 
+                </FadeIn><br/>
+                <FadeIn delay={1500} transitionDuration={2000}>
+                    <div className="projects-tab">
 
-            <div className="Card">
-                <h2>KeyBlogs UI</h2>
-                <h4>JavaScript, Reactjs, Axios, Loading Skeletons</h4>
-                <p>KeyBlogs UI is a blogging web app's User Interface made with Reactjs Framework.
-                  API connections are made using Axios and Loading Skeletons are also used.
-                </p>
-                <div className="card-lang">
-                    <img src={JavaScript} alt="JavaScript" /> &nbsp;
-                    <img src={ReactLogo} alt="React" />
-                </div><br />
-                <div className="card-nav">
-                    <div className="card-nav-left">
-                        <a href="https://github.com/nandan-unni/KeyBlogs-React-Frontend">View Source Code</a>
-                    </div>
-                    <div className="card-nav-right">
-                        <a href="https://keyblogs.web.app">View Project</a>
-                    </div>
-                </div>
+                        <div className="tab-section">
+                            <div className="Card">
+                                <h2>KeyBlogs API</h2>
+                                <h4>Python, Django, Django Rest Framework, PostgreSQL, SQLite3</h4>
+                                <p>KeyBlogs API is a blogging web app's API made with Django REST Framework</p>
+                                <div className="card-lang">
+                                    <img src={Python} alt="Python" /> &nbsp; 
+                                    <img src={Django} alt="Django" />
+                                </div><br />
+                                <div className="card-nav">
+                                    <div className="card-nav-left">
+                                        <a href="https://github.com/nandan-unni/KeyBlogs-Django-Backend">View Source Code</a>
+                                    </div>
+                                    <div className="card-nav-right">
+                                        <a href="https://keyblogs.web.app">View Project</a>
+                                    </div>
+                                </div>
+                            </div> &nbsp;  {/* 1st card end */}
+
+                            <div className="Card">
+                                <h2>KeyBlogs UI</h2>
+                                <h4>JavaScript, Reactjs, Axios, Loading Skeletons</h4>
+                                <p>KeyBlogs UI is a blogging web app's UI made with Reactjs Framework.</p>
+                                <div className="card-lang">
+                                    <img src={JavaScript} alt="JavaScript" /> &nbsp;
+                                    <img src={ReactLogo} alt="React" />
+                                </div><br />
+                                <div className="card-nav">
+                                    <div className="card-nav-left">
+                                        <a href="https://github.com/nandan-unni/KeyBlogs-React-Frontend">View Source Code</a>
+                                    </div>
+                                    <div className="card-nav-right">
+                                        <a href="https://keyblogs.web.app">View Project</a>
+                                    </div>
+                                </div>
+                            </div> {/* 2nd card end */}
+                        </div><br/> {/* 1st tab-section end */}
+
+                        <div className="tab-section"> {/* 2nd tab-section start */}
+
+                            <div className="Card"> {/* 3rd card start */}
+                                <h2>Instagram API Clone</h2>
+                                <h4>Python, Django, Django Rest Framework, Simple-jwt, SQLite3</h4>
+                                <p>The clone of Instagram API made with Django REST Framework</p>
+                                <div className="card-lang">
+                                    <img src={Python} alt="Python" /> &nbsp; 
+                                    <img src={Django} alt="Django" />
+                                </div><br />
+                                <div className="card-nav">
+                                    <div className="card-nav-left">
+                                        <a href="https://github.com/nandan-unni/instagram-api-clone">View Source Code</a>
+                                    </div>
+                                    <div className="card-nav-right">
+                                        <a href="https://github.com/nandan-unni/nandan-unni">View Project</a>
+                                    </div>
+                                </div>
+                            </div> &nbsp;  {/* 3rd card end */}
+
+                            <div className="Card"> {/* 4th card start */}
+                                <h2>React Calculator</h2>
+                                <h4>JavaScript, React</h4>
+                                <p>A simple Calculator made with Reactjs framwork</p>
+                                <div className="card-lang">
+                                    <img src={JavaScript} alt="JavaScript" /> &nbsp;
+                                    <img src={ReactLogo} alt="React" />
+                                </div><br />
+                                <div className="card-nav">
+                                    <div className="card-nav-left">
+                                        <a href="https://github.com/nandan-unni/React-Calculator">View Source Code</a>
+                                    </div>
+                                    <div className="card-nav-right">
+                                        <a href="https://nqcalculator.web.app">View Project</a>
+                                    </div>
+                                </div>
+                            </div> {/* 4th card end */}
+
+                        </div> {/* 2nd tab-section end */}
+                    </div> {/* Projects Tab end */}
+                </FadeIn>
+                <Footer></Footer>
             </div>
-          </div><br/>
-
-
-          <div className="tab-section">
-
-            <div className="Card">
-                <h2>Instagram API Clone</h2>
-                <h4>Python, Django, Django Rest Framework, Simple-jwt, SQLite3</h4>
-                <p>The clone of Instagram Application Program Interface (API) made with Django REST Framework</p>
-                <div className="card-lang">
-                    <img src={Python} alt="Python" /> &nbsp; 
-                    <img src={Django} alt="Django" />
-                </div><br />
-                <div className="card-nav">
-                    <div className="card-nav-left">
-                        <a href="https://github.com/nandan-unni/instagram-api-clone">View Source Code</a>
-                    </div>
-                    <div className="card-nav-right">
-                        <a href="https://github.com/nandan-unni/nandan-unni">View Project</a>
-                    </div>
-                </div>
-            </div> &nbsp; 
-
-            <div className="Card">
-                <h2>React Calculator</h2>
-                <h4>JavaScript, React</h4>
-                <p>A simple Calculator made with Reactjs framwork</p>
-                <div className="card-lang">
-                    <img src={JavaScript} alt="JavaScript" /> &nbsp;
-                    <img src={ReactLogo} alt="React" />
-                </div><br />
-                <div className="card-nav">
-                    <div className="card-nav-left">
-                        <a href="https://github.com/nandan-unni/React-Calculator">View Source Code</a>
-                    </div>
-                    <div className="card-nav-right">
-                        <a href="https://nqcalculator.web.app">View Project</a>
-                    </div>
-                </div>
-            </div>
-
-          </div>
-
-
-        </div>
-        <Footer></Footer>
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default Home;
