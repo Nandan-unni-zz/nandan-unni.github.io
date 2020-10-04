ff45384d782f96ff96db49a6d482223b7ba726d3
nandan-unni <asnqln@gmail.com>
2020-10-04 21:22:21 +0530

    Updates

---
+++ precache-manifest.9ee6a9ad068a076b19976c92749de15d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9848cb99f009b251251174809c432f94",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1dece95b965027c15b5b",
+    "url": "/static/css/main.d5cfcc27.chunk.css"
+  },
+  {
+    "revision": "3804ad022a6b6ba06b06",
+    "url": "/static/js/2.dd04e839.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dd04e839.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1dece95b965027c15b5b",
+    "url": "/static/js/main.d9587a65.chunk.js"
+  },
+  {
+    "revision": "2d4daf1fa9867e639b40",
+    "url": "/static/js/runtime-main.ec11ef69.js"
+  },
+  {
+    "revision": "e688e3456a945f04dfc19430e7dd7559",
+    "url": "/static/media/background.e688e345.jpg"
+  },
+  {
+    "revision": "b99145fd253da16c3c45c493277b8ea0",
+    "url": "/static/media/nandanunni.b99145fd.jpg"
+  },
+  {
+    "revision": "cf280d5999aaab1daa9525745bbd882d",
+    "url": "/static/media/react.cf280d59.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bc49c112c82a2308d61039c438da6b99.js"
+  "/precache-manifest.9ee6a9ad068a076b19976c92749de15d.js"
 );
 
 self.addEventListener('message', (event) => {
