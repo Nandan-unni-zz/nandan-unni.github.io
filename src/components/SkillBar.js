bba55714f9b5e2918f3c851beb822e7b19c48f55
nandan-unni <asnqln@gmail.com>
2020-10-05 12:01:09 +0530

    v1.1

---
+++ src/components/SkillBar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import './SkillBar.css';
+
+class SkillBar extends Component {
+    render() {
+        return (
+            <div className="SkillBar"></div>
+        );
+    }
+}
+
+export default SkillBar
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,15 +11,15 @@ import CardC from '../components/CardC';
 import CardD from '../components/CardD';
 
 import ProfilePicture from '../images/nandanunni.jpg';
-import Python from '../images/python.png';
-import Django from '../images/django.png';
-import JavaScript from '../images/javascript.png';
+import PythonLogo from '../images/python.png';
+import DjangoLogo from '../images/django.png';
+import JavaScriptLogo from '../images/javascript.png';
 import ReactLogo from '../images/react.png';
-import Golang from '../images/go.png';
-import Cpp from '../images/cpp.png';
-import C from '../images/c.png';
-import HTML from '../images/html.png';
-import CSS from '../images/css.png';
+import GolangLogo from '../images/go.png';
+import CPPLogo from '../images/cpp.png';
+import CLogo from '../images/c.png';
+import HTMLLogo from '../images/html.png';
+import CSSLogo from '../images/css.png';
 
 class Home extends Component {
     render() {
@@ -27,11 +27,12 @@ class Home extends Component {
             <div className="Home">
                 <Header></Header>
 
+
                 {/* _____ ABOUT Section _____ */}
                 <FadeIn delay={1500} transitionDuration={1000}>
-                    <div className="projects-header" id="about">
+                    <div className="section-header" id="about">
                         <i class="fa fa-2x fa-diamond"><b>&nbsp;About</b></i><br/>
-                        <div className="projects-line"></div>
+                        <div className="section-line"></div>
                     </div>
                 </FadeIn><br/>
                 <div className="about">
@@ -48,20 +49,21 @@ class Home extends Component {
                 </div><br /><br /><br />
                 {/* _____ ABOUT Section end _____ */}
 
+
                 {/* _____ PROJECTS Section _____ */}
                 <FadeIn delay={3000} transitionDuration={1000}>
-                    <div className="projects-header" id="projects">
+                    <div className="section-header" id="projects">
                         <i class="fa fa-2x fa-diamond"><b>&nbsp;Projects</b></i><br/>
-                        <div className="projects-line"></div>
+                        <div className="section-line"></div>
                     </div>
                 </FadeIn><br/>
                 <FadeIn delay={3000} transitionDuration={2000}>
                     <div className="projects-tab">
-                        <div className="tab-section">
+                        <div className="tab-block">
                             <CardA></CardA> &nbsp;
                             <CardB></CardB> &nbsp;
                         </div><br/>
-                        <div className="tab-section">
+                        <div className="tab-block">
                             <CardC></CardC> &nbsp;
                             <CardD></CardD> &nbsp;
                         </div><br />
@@ -69,31 +71,34 @@ class Home extends Component {
                 </FadeIn><br /><br />
                 {/* _____ PROJECTS Section end _____ */}
 
+
                 {/* _____ SKILLS Section _____ */}
                 <FadeIn delay={3000} transitionDuration={1000}>
-                    <div className="projects-header" id="projects">
+                    <div className="section-header" id="projects">
                         <i class="fa fa-2x fa-diamond"><b>&nbsp;Skills</b></i><br/>
-                        <div className="projects-line"></div><br/>
+                        <div className="section-line"></div><br/>
                         <div className="skills-lang">
-                            <img src={Python} alt="Python" /> &nbsp; 
-                            <img src={Django} alt="Django" /> &nbsp; 
-                            <img src={JavaScript} alt="JavaScript" /> &nbsp; 
+                            <img src={PythonLogo} alt="Python" /> &nbsp; 
+                            <img src={DjangoLogo} alt="Django" /> &nbsp; 
+                            <img src={JavaScriptLogo} alt="JavaScript" /> &nbsp; 
                             <img src={ReactLogo} alt="React" /> &nbsp; 
-                            <img src={Golang} alt="Go" /> &nbsp; 
-                            <img src={Cpp} alt="C++" /> &nbsp; 
-                            <img src={C} alt="C" /> &nbsp; 
-                            <img src={HTML} alt="HTM5" /> &nbsp; 
-                            <img src={CSS} alt="CSS3" />
+                            <img src={GolangLogo} alt="Go" /> &nbsp; 
+                            <img src={CPPLogo} alt="C++" /> &nbsp; 
+                            <img src={CLogo} alt="C" /> &nbsp; 
+                            <img src={HTMLLogo} alt="HTM5" /> &nbsp; 
+                            <img src={CSSLogo} alt="CSS3" />
                         </div><br />
                     </div>
                 </FadeIn><br/><br /><br />
                 {/* _____ SKILLS Section end _____ */}
+
 
                 {/* _____ CONTACT Section _____ */}
                 <FadeIn delay={3000} transitionDuration={1000}>
                     <div id="contact"><Footer></Footer></div>
                 </FadeIn>
                 {/* _____ CONTACT Section end _____ */}
+
 
             </div>
         );
