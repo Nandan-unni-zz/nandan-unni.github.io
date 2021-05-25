b0d526b0b38afcfe63e320263bdacb824c0da4ed
Nandan-unni <asnqln@gmail.com>
2021-05-25 15:40:54 +0530

    header done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,24 @@
+const setEnvironment = () => {
+  if (localStorage.getItem("theme") === "lightMode")
+    document.body.classList.replace("darkMode", "lightMode");
+  else document.body.classList.replace("lightMode", "darkMode");
+};
+
 const toggleTheme = () => {
-  let body = document.getElementById("body");
-  if (body.className.includes("dark")) body.classList.replace("dark", "light");
-  else body.classList.replace("light", "dark");
+  if (document.body.className.includes("darkMode")) {
+    document.body.classList.replace("darkMode", "lightMode");
+    localStorage.setItem("theme", "lightMode");
+  } else {
+    document.body.classList.replace("lightMode", "darkMode");
+    localStorage.setItem("theme", "darkMode");
+  }
+};
+
+const toggleMenu = () => {
+  let menuBtn = document.getElementById("menuToggleBtn");
+  if (menuBtn.className.includes("openMenu")) {
+    menuBtn.classList.replace("openMenu", "closedMenu");
+  } else {
+    menuBtn.classList.replace("closedMenu", "openMenu");
+  }
 };
